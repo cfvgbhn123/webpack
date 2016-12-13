603fd2b622c396aadb1c771ea019704cf0ea20ea
PC201612061110\Administrator <cfvgbhn123@126.com>
2016-12-13 17:17:35 +0800

    cc

---
+++ src/js/bundle.js
@@ -0,0 +1,82 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var $ = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./jquery\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var Vue = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./vue\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var app = __webpack_require__(1);
+	console.log(app);
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(module) {var app = new Vue({
+	    el: '#app',
+	    data: {
+	        message: 'Hello Vue!'
+	    }
+	})
+	module.export = app;
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)(module)))
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = function(module) {
+		if(!module.webpackPolyfill) {
+			module.deprecate = function() {};
+			module.paths = [];
+			// module.parent = undefined by default
+			module.children = [];
+			module.webpackPolyfill = 1;
+		}
+		return module;
+	}
+
+
+/***/ }
+/******/ ]);
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,4 +1,4 @@
-var $ = require("../jquery");
-var Vue = require("../vue");
-var app = require("main");
+var $ = require("./jquery");
+var Vue = require("./vue");
+var app = require("./main");
 console.log(app);
